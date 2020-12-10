9199a5da167732fb6ea84b872a372656ea1140e6
Nikolay Potapov <rnjv2015@yandex.ru>
2020-12-11 01:00:27 +0300

    add methods for Option

--- src/main/scala/module1/App.scala
+++ src/main/scala/module1/App.scala
@@ -10,5 +10,8 @@ object App {
 
   def main(args: Array[String]): Unit = {
 
+    println(opt.Option.None.orElse(4))
+    println(opt.Option.Some(2).filter(x=>5>x))
+    println(opt.Option.Some(2).zip(opt.Option.Some(3)).get)
   }
 }
